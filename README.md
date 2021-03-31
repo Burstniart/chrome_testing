61b25762f6d9cc3fb80991c8ec129d9a43eee444
Busrtniart <v4gr33n@hotmail.com>
2021-03-31 13:22:35 -0600

    03/31/2021 action/scripting/command/tabs/activeTab

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 # pushing 1
 # switched to manifest v3 because of error in background.js wich didn't recognized chrome.scripting.executeScript
 # omited console.log for i dont know how to call it so it returns the tab.url
+# edit 31/03/2021
+# checked commands from "api/examples/pin_tab"
+# succesfully injected background.js for manifest v3
+# succesfully added commands for shortcut function execution
+# fixed misplaced console.log ( Error was looking at foreground log while looking for a console.log being executed in the background.js, resolved by opening the extension service worker aka "background.js" dev tools from the extensions panel)
+# now i can see the url from the tab my extension is executed from by clicking it with chrome.action.onClicked.addListener
+
+# experience problems for executing another command from a shortcut, further investigation is required
--- background.js
+++ background.js
@@ -1,10 +1,32 @@
 function greennenPage() {
     document.body.style.backgroundColor = "green";
 }
+function alertMe() {
+    alert("Activated a shortcut")
+}
 
 chrome.action.onClicked.addListener((tab) => {
+    console.log('Turning ' + tab.url + ' green!');
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
         function: greennenPage,
     });
   });
+
+  chrome.commands.onCommand.addListener(function(command) {
+      if (command == "toggle-pin") {
+          // Get the currently selected tab
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            // Toggle the pinned status
+            var current = tabs[0]
+            chrome.tabs.update(current.id, {'pinned': !current.pinned});
+          })
+      }// currently experimenting dificulties executing another shortcut command
+      /* else if (command == "alert_shortcut") {
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+              var current = tabs[0]
+              chrome.tabs.update(current.id, {'pinned': !current.pinned});
+          })
+      } */
+  });
+
