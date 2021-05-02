c86a124f724c626c187bb039751fad585f81b052
Busrtniart <v4gr33n@hotmail.com>
2021-05-01 23:06:37 -0500

    ok

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Test
 # Testing commit and push commands for Git from VS Code
 # continue testing page action from this branch
+# 04/04/2021
+Files currently in page action brange
+# looking for a way to execute script on v2 succesfully
--- background.js
+++ background.js
@@ -1,30 +1,30 @@
-function greennenPage() {
-    document.body.style.backgroundColor = "green";
-}
 
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: greennenPage
-    })
-})
 
 chrome.runtime.onInstalled.addListener(function() {
-    //Replace all rules...
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        //With a new rule...
         chrome.declarativeContent.onPageChanged.addRules([
             {
-                // That fires when a page's URL contains 'github'...
                 conditions: [
                     new chrome.declarativeContent.PageStateMatcher({
                         pageUrl: { urlContains: 'g'},
                     })
                 ],
-                // And shows the extension's page action
-                actions: [ new crome.declarativeContent.ShowPageAction()]
-            }
+                actions: [ new chrome.declarativeContent.ShowPageAction() ]
+            },
         ]);
     });
 });
 
+function greennenPage() {
+    document.body.style.backgroundColor = "green";
+}
+
+
+
+chrome.action.onClicked.addListener((tab) => {
+    console.log('Turning ' + tab.url + ' green!');
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        function: greennenPage,
+    });
+  });
